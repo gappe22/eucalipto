3061c001fc2607623ac854ee1d4047b1da467d40
andry <andry0powa@gmail.com>
2021-11-15 23:32:57 +0100

    attached bot to discord APIs and modified README file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Eucalipto
 most human bot on discord.
+
+## Setup
+install dependencies with `npm install`.
+create config.json file with secret token.
--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-// main file
+const { Client, Intents } = require('discord.js');
+const { secretToken } = require('../config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('eucalipto started!')
+});
+
+client.login(secretToken);
